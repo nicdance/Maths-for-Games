5564405b8df8f2c77139e55c771b058f9e32883f
Nicole <nicoledance@chaosshard.com.au>
2020-05-07 19:14:07 +1000

    Setting up Base Maths Project.

---
+++ C# Unit Test - Student Copy/MathClasses/Colour.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathClasses
+{
+    public class Colour
+    {
+        public UInt32 colour;
+
+        // Can update
+        public Colour()
+        {
+            colour = 0;
+        }
+
+    }
+}
---
+++ C# Unit Test - Student Copy/MathClasses/Vector3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace MathClasses
+{
+    public class Vector3
+    {
+        public float x, y, z;
+
+        public Vector3()
+        {
+            x = 0;
+            y = 0;
+            z = 0;
+        }
+
+        public Vector3(int _x, int _y, int _z) {
+            x = _x;
+            y = _y;
+            z = _z;
+        }
+
+    }
+}
